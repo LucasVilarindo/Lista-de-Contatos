27049280814549c476107d38c38734148129ac0c
LucasVilarindo <lucasvilarindo@outlook.com>
2024-05-18 19:55:58 -0300

    first commit

---
+++ src/ListaDeContatos.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import './ListaDeContatos.css'
+function ListaDeContatos() { 
+
+  const [contatos, setContatos] = useState([]); 
+
+  const [nome, setNome] = useState(''); 
+
+  const [telefone, setTelefone] = useState(''); 
+
+  
+
+  const adicionarContato = () => { 
+
+    if (nome && telefone) { 
+
+      setContatos([...contatos, { nome, telefone }]); 
+
+      setNome(''); 
+
+      setTelefone(''); 
+
+    } 
+
+  }; 
+
+  
+
+  return ( 
+
+    <div className='section'> 
+
+      <h2>Lista de Contatos</h2> 
+
+      <div> 
+
+        <input 
+
+          type="text" 
+
+          value={nome} 
+
+          onChange={(e) => setNome(e.target.value)} 
+
+          placeholder="Nome" 
+
+        /> 
+
+        <input 
+
+          type="text" 
+
+          value={telefone} 
+
+          onChange={(e) => setTelefone(e.target.value)} 
+
+          placeholder="Telefone" 
+
+        /> 
+
+        <button onClick={adicionarContato}>Adicionar Contato</button> 
+
+      </div> 
+
+      <ul> 
+
+        {contatos.map((contato, index) => ( 
+
+          <li key={index}> 
+
+            <strong>{contato.nome}:</strong> {contato.telefone} 
+
+          </li> 
+
+        ))} 
+
+      </ul> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default ListaDeContatos; 
